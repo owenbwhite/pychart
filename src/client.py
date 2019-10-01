fca1aa639e4c26f775e87e9e12c52a5da7477ae6
Owen White <me@owenwhite.info>
2019-10-01 11:32:52 -0400

    [PyChart] Minor Changes

--- src/client.py
+++ src/client.py
@@ -7,15 +7,16 @@ class ChartmetricException(Exception):
 
     def __init__(self, http_status, code, msg, headers=None):
 
-
-        self.http_status = http_status
-        self.code = code
-        self.msg = msg
-        if headers is None:
-            headers = {}
-        self.headers = headers
+        pass
+        # self.http_status = http_status
+        # self.code = code
+        # self.msg = msg
+        # if headers is None:
+        #     headers = {}
+        # self.headers = headers
 
     def __str__(self):
+        print('help')
         return 'http status: {0}, code:{1} - {2}'.format(
             self.http_status, self.code, self.msg)
 
